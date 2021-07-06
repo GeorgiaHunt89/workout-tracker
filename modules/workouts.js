6df2e0274fc17e5f03494c9b5a68e6f17804b61e
Georgia Hunt <georgialhunt89@gmail.com>
2021-07-06 15:19:56 +0930

    Added aggregate add fields method for total dur.

--- modules/workouts.js
+++ modules/workouts.js
@@ -1,56 +1,69 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutsSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-
-  exercises: [
-    {
-      name: {
-        type: String,
-        trim: true,
-        required: "Please enter the name of the exercise",
-      },
-
-      type: {
-        type: String,
-        trim: true,
-        required: "Please enter the type of the exercise",
-      },
-
-      duration: {
-        type: Number,
-        required: "Please enter the duration of the exercise",
-      },
-
-      weight: {
-        type: Number,
-        required: "Please enter the weight used",
-        default: 0,
-      },
-
-      reps: {
-        type: Number,
-        required: "Please enter the number of reps completed,",
-        default: 0,
-      },
-
-      sets: {
-        type: Number,
-        required: "Please enter the number of sets completed",
-        default: 0,
-      },
-
-      distance: {
-        type: Number,
-        required: "Please enter the distance completed",
-        default: 0,
-      },
+const workoutsSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
+
+    exercise: [
+      {
+        name: {
+          type: String,
+          trim: true,
+          required: "Please enter the name of the exercise",
+        },
+
+        type: {
+          type: String,
+          trim: true,
+          required: "Please enter the type of the exercise",
+        },
+
+        duration: {
+          type: Number,
+          required: "Please enter the duration of the exercise",
+        },
+
+        weight: {
+          type: Number,
+          required: "Please enter the weight used",
+          default: 0,
+        },
+
+        reps: {
+          type: Number,
+          required: "Please enter the number of reps completed,",
+          default: 0,
+        },
+
+        sets: {
+          type: Number,
+          required: "Please enter the number of sets completed",
+          default: 0,
+        },
+
+        distance: {
+          type: Number,
+          required: "Please enter the distance completed",
+          default: 0,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutsSchema.virtual("totalDuration").get(function () {
+  return this.exercise.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const workouts = mongoose.model("Workouts", workoutsSchema);
