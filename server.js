0b7cf06434766a8d356d7fa307aec7b9cf5cc88b
Georgia Hunt <georgialhunt89@gmail.com>
2021-07-06 15:10:35 +0930

    Depen. for mongoose & logger plus api routes

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-const express = require('express');
-const routes = require('./routes/api');
-// import sequelize connection
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
+// Setting up the express port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Allowing logger use
+app.use(logger("dev"));
+
+// Allowing data parsing for Express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('routes/api.js');
+// Mongoose connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
-// sync sequelize models to the database, then turn on the server
+// Routes required for HTML and API routes
+app.use(require("routes/apiRoutes.js"));
+app.use(require("routes/htmlRoutes.js"));
+
+// Add listener for PORT
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
