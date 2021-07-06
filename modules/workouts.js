1ad74bab7dbc5507a0509bd88ba2e3fc2bd3a8a2
Georgia Hunt <georgialhunt89@gmail.com>
2021-07-06 14:41:37 +0930

    Added date.now and default values to model

--- modules/workouts.js
+++ modules/workouts.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const workoutsSchema = new Schema({
   day: {
     type: Date,
+    default: Date.now,
   },
 
   exercises: [
@@ -28,21 +29,25 @@ const workoutsSchema = new Schema({
       weight: {
         type: Number,
         required: "Please enter the weight used",
+        default: 0,
       },
 
       reps: {
         type: Number,
         required: "Please enter the number of reps completed,",
+        default: 0,
       },
 
       sets: {
         type: Number,
         required: "Please enter the number of sets completed",
+        default: 0,
       },
 
       distance: {
         type: Number,
         required: "Please enter the distance completed",
+        default: 0,
       },
     },
   ],
