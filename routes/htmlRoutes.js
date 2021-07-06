c69a914d0e14bda07e86d37647f88540dcb16a8e
Georgia Hunt <georgialhunt89@gmail.com>
2021-07-06 15:05:14 +0930

    Added depend. & route for index / exercise / stats

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+// Dependencies
+const path = require("path");
+const router = require("express").Router();
+
+// Get route index.html
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// Get route for exercise
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// Get route for stats
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
